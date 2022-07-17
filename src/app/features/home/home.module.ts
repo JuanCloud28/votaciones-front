8a66ce58d9e6aaf2d46564acae264d4bf16af033
JuanCloud28 <juanrodriguezf18@gmail.com>
2022-07-17 00:13:22 -0500

    Angular Material added, Home feature created and Material custom theme added

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent
+  ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatButtonModule
+  ],
+  exports: [
+    HomePageComponent
   ]
 })
 export class HomeModule { }
